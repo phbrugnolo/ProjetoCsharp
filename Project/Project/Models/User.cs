a445d48035619b86d39730e2a23a9eec8b5487b7
Weslley <weslley_ss@live.com>
2024-05-14 19:15:45 -0300

    criação classe de usuarios

---
+++ Project/Project/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace Project;
+
+public class User
+{
+    public User(string nome)
+    {
+        Nome = nome;
+        Id = Guid.NewGuid().ToString();
+        Vitoria = 0;
+        Derrota = 0;
+
+    }
+
+    private string? Nome { get; set; }
+    private int Vitoria { get; set; }
+    private int Derrota { get; set; }
+    private string? Id { get; set; }
+
+
+}
