3a7d40a8ef3281a93d228cb5c69625b9eb90686f
C4d5 <163066432+C4d5@users.noreply.github.com>
2024-05-16 20:55:57 -0300

    implementando lógica da batalha

--- Project/Project/Models/Battle.cs
+++ Project/Project/Models/Battle.cs
@@ -2,8 +2,34 @@ namespace Project;
 
 public class Battle
 {
-    public Battle(User user)
+    public Battle(string jogada)
     {
+        Jogada = jogada.ToLower().Trim();
 
     }
+
+    public string Batalhar()
+    {
+        string[] opcoes = { "pedra", "papel", "tesoura" };
+        Random random = new Random();
+        int indiceMaquina = random.Next(0, 3);
+        string jogadaMaquina = opcoes[indiceMaquina];
+
+        if (Jogada == jogadaMaquina)
+        {
+            return "Empate!";
+        }
+        else if ((Jogada == "pedra" && jogadaMaquina == "tesoura") ||
+                 (Jogada == "papel" && jogadaMaquina == "pedra") ||
+                 (Jogada == "tesoura" && jogadaMaquina == "papel"))
+        {
+            return "Você venceu!";
+        }
+        else
+        {
+            return "Você perdeu!";
+        }
+    }
+
+    public string Jogada { get; set; }
 }
