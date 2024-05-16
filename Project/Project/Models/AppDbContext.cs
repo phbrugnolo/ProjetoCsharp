67b70776bf90532c140fd9a4fa7e861af97248cb
phbrugnolo <phbrugnolo@gmail.com>
2024-05-16 20:39:33 -0300

    Adicionando o torenio as migrações

--- Project/Project/Models/AppDbContext.cs
+++ Project/Project/Models/AppDbContext.cs
@@ -4,6 +4,7 @@ using Project;
 public class AppDbContext : DbContext
 {
     public DbSet<User> Users { get; set; }
+    public DbSet<Torneio> Torneios { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
